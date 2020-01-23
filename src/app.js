60f1a8af5e7205c961b9708ff1bafe3b6b42638a
Volodymyr <klischvolodimir@gmail.com>
2020-01-23 18:19:48 +0200

    creating server

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const schoolRouter = require('./routers/index')
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(schoolRouter);
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`)
+})
