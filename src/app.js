31b7bd8750ad7b3ce3f9579645c86a9ecba97387
Volodymyr <klischvolodimir@gmail.com>
2020-01-25 21:25:26 +0200

    added teacherRouter

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const schoolRouter = require('./routers/index')
+const schoolRouter = require('./routers/school');
+const teacherRouter = require('./routers/teacher');
 require('./db/mongoose');
 
 const app = express();
@@ -7,6 +8,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(schoolRouter);
+app.use(teacherRouter);
 
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`)
