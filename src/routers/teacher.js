0fb9837139d7b685a671278d91af734b57e0a80f
Volodymyr <klischvolodimir@gmail.com>
2020-01-26 01:11:23 +0200

    has problem with auth

---
+++ src/routers/teacher.js
@@ -0,0 +1,103 @@
+const express = require('express');
+const auth = require('../middleware/auth');
+const Teacher = require('../models/teacher');
+const router = new express.Router();
+
+router.post('/teacher', async (req, res) => {
+    const teacher = new Teacher(req.body);
+
+    try {
+        await teacher.save();
+        const token = await teacher.generateAuthToken();
+        res.status(201).send({teacher, token});
+    } catch (error) {
+        res.status(400).send(error)
+    }
+});
+
+router.post('/teacher/login', async (req, res) => {
+    try {
+        const teacher = await Teacher.findByCredentials(req.body.email, req.body.password);
+        const token = await teacher.generateAuthToken();
+        res.send({teacher, token});
+    } catch (error) {
+        res.status(400).send();
+    }
+});
+
+router.post('/teacher/logout', auth, async (req, res) => {
+    try {
+        req.teacher.tokens =  req.teacher.tokens.filter((token) => {
+            return token.token != req.token;
+        });
+
+        await req.teacher.save();
+        res.status(200).send('You logged out!');
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+router.post('/teacher/logoutAll', auth, async (req, res) => {
+    try {
+        req.teacher.tokens = [];
+        await req.teacher.save();
+        res.send('Logged out All!');
+    } catch (error) {
+        res.status(500).send();
+    }
+});
+
+router.patch('/teacher/me', auth, async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['email', 'password', 'name'];
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+    if (!isValidOperation) {
+        res.status(400).send({error: 'Invalid updates!'});
+    }
+
+    try {
+       const teacher = await Teacher.findById(req.params.id);
+
+       updates.forEach( (update) => teacher[update] = req.body[update]);
+       await teacher.save();
+
+        if (!teacher) {
+            return res.status(404).send()
+        }
+
+        res.send(teacher);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+})
+
+router.get('/teacher/me', auth, async (req, res) => {
+    res.send(req.user);
+});
+
+router.get('/teacher/:id', auth, async (req, res) => {
+    const _id = req.params.id;
+    try {
+        const teacher = await Teacher.findById(_id);
+        if (!teacher) {
+            return res.status(404).send();
+        }
+
+        res.status(200).send(teacher);
+    } catch (e) {
+        res.status(500).send({e: 'Not found'});
+    }
+});
+
+router.delete('/teacher/me', auth, async (req, res) => {
+    try {
+        await req.user.remove();
+        res.send(req.user);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+});
+
+module.exports = router;
