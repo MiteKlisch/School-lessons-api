48e6f3d4b899ff6ef7421bd592f7c67b30b46936
Volodymyr <klischvolodimir@gmail.com>
2020-01-26 23:26:20 +0200

    added 'pupilRouter' and 'groupRouter'

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const lessonRouter = require('./routers/lesson');
 const teacherRouter = require('./routers/teacher');
+const pupilRouter = require('./routers/pupils');
+const groupRouter = require('./routers/group');
 require('./db/mongoose');
 
 const app = express();
@@ -9,6 +11,8 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(lessonRouter);
 app.use(teacherRouter);
+app.use(pupilRouter);
+app.use(groupRouter);
 
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`)
