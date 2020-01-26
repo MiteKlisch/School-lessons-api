4e919e998d0fdc38cd45ecd2346796a5766996cb
Volodymyr <klischvolodimir@gmail.com>
2020-01-26 22:45:25 +0200

    fixed incorrect names of values

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,15 +5,15 @@ const Teacher = require('../models/teacher');
 const auth = async (req, res, next) => {
    try {
         const token = req.header('Authorization').replace('Bearer ', '');
-        const decoded = jwt.verify(token, 'secretword');
-        const user = await Teacher.findOne({ _id: decoded._id, 'tokens.token': token });
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const teacher = await Teacher.findOne({ _id: decoded._id, 'tokens.token': token });
 
-        if (!user) {
+        if (!teacher) {
             throw new Error();
         }
         
         req.token = token;
-        req.user = user;
+        req.teacher = teacher;
         next();
    } catch (e) {
        res.status(401).send({error: 'Please authenticate.'})
