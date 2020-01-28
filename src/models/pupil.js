3fea39fe109486df2852456cfcb0d4ffffc36929
Volodymyr <klischvolodimir@gmail.com>
2020-01-28 20:04:28 +0200

    fixed errors (Eslint)

--- src/models/pupil.js
+++ src/models/pupil.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+
 const pupilSchema = new mongoose.Schema({
   name: {
     type: String,
--- src/routers/lesson.js
+++ src/routers/lesson.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const auth = require('../middleware/auth')
+const auth = require('../middleware/auth');
 // const Lesson = require('../models/lesson');
 const lessonController = require('../controllers/lessonController');
 
